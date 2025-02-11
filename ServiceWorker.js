7026adf4fa1793756a1d5cb52a2ed8355d5d62f2
shaheryar-inceptive <shaheryar@in-ceptive.com>
2025-02-11 15:48:13 +0400

    service worker fixed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,43 @@
 const cacheName = "DefaultCompany-My project-0.1.0";
 const contentToCache = [
-    "Build/YugaBaycAR.loader.js",
-    "Build/YugaBaycAR.framework.js.unityweb",
-    "Build/YugaBaycAR.data.unityweb",
-    "Build/YugaBaycAR.wasm.unityweb",
-    "TemplateData/style.css"
-
+  "Build/YugaBaycAR.loader.js",
+  "Build/YugaBaycAR.framework.js.unityweb",
+  "Build/YugaBaycAR.data.unityweb",
+  "Build/YugaBaycAR.wasm.unityweb",
+  "TemplateData/style.css",
 ];
 
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
+self.addEventListener("install", function (e) {
+  console.log("[Service Worker] Install");
+
+  e.waitUntil(
+    (async function () {
       const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
+      console.log("[Service Worker] Caching all: app shell and content");
       await cache.addAll(contentToCache);
-    })());
+    })()
+  );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    (async function () {
       let response = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+      if (response) {
+        return response;
+      }
 
       response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+
+      // Only cache GET requests
+      if (e.request.method === "GET") {
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+      }
+
       return response;
-    })());
+    })()
+  );
 });
